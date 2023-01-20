07009de74f31ae35b405efa4982d970465e11ff6
PrajwalRanaMagar <prajwalrana54@gmail.com>
2023-01-20 22:58:31 +0545

    Section 17: Adding Http and Forms to the food order app

--- 11-Practice Project Building a Food Order App/src/store/CartProvider.js
+++ 11-Practice Project Building a Food Order App/src/store/CartProvider.js
@@ -1,6 +1,6 @@
-import { useReducer } from "react";
+import { useReducer } from 'react';
 
-import CardContext from "./cart-context";
+import CartContext from './cart-context';
 
 const defaultCartState = {
   items: [],
@@ -8,30 +8,26 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === "ADD") {
-    const updateItems = state.items.concat(action.item);
+  if (action.type === 'ADD') {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
-
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
     if (existingCartItem) {
-      const updateItem = {
+      const updatedItem = {
         ...existingCartItem,
-        amount: existingCartItem.amount + action.item.amount
+        amount: existingCartItem.amount + action.item.amount,
       };
       updatedItems = [...state.items];
-      updateItems[existingCartItemIndex] = updateItem;
+      updatedItems[existingCartItemIndex] = updatedItem;
     } else {
       updatedItems = state.items.concat(action.item);
     }
-
-    
 
     return {
       items: updatedItems,
@@ -39,25 +35,28 @@ const cartReducer = (state, action) => {
     };
   }
   if (action.type === 'REMOVE') {
-   
-
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
-      const existingItem = state.items[existingCartItemIndex];
-      const updatedTotalAmount = state.totalAmount - existingItem.price;
-      let updatedItems;
-      if (existingItem.amount === 1){
-        updatedItems = state.items.filter(item => item.id !== action.id);
-      }else {
-        const updatedItem = {...existingItem, amount: existingItem.amount - 1 };
-        updatedItems = [...state.items];
-        updatedItem[existingCartItemIndex] = updatedItem;
-      }
-      return{
-        items: updatedItems,
-        totalAmount: updatedTotalAmount
-      };
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
   }
 
   return defaultCartState;
@@ -70,11 +69,15 @@ const CartProvider = (props) => {
   );
 
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({ type: "ADD", item: item });
+    dispatchCartAction({ type: 'ADD', item: item });
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: "REMOVE", id: id });
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
+
+  const clearCartHandler = () => {
+    dispatchCartAction({type: 'CLEAR'});
   };
 
   const cartContext = {
@@ -82,11 +85,13 @@ const CartProvider = (props) => {
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
+
   return (
-    <CardContext.Provider value={cartContext}>
+    <CartContext.Provider value={cartContext}>
       {props.children}
-    </CardContext.Provider>
+    </CartContext.Provider>
   );
 };
 
